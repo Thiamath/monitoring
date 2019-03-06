458d8fd696d6c9bdcfb0b180b7082a173032748d
Giel de Nijs <giel@daisho.group>
2019-03-06 11:48:25 +0100

    [NP-608] Infrastructure monitor slave server with no handlers.

---
+++ internal/app/slave/config.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Nalej - All Rights Reserved
+ */
+
+// Wrapper for the configuration properties.
+
+package slave
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nalej/derrors"
+	"github.com/rs/zerolog/log"
+)
+
+// Config struct for the API service.
+type Config struct {
+	// Port where the API service will listen requests.
+	Port int
+	// Path to kubeconfig
+	Kubeconfig string
+	// Running inside Kubernetes cluster
+	InCluster bool
+}
+
+// Validate the configuration.
+func (conf *Config) Validate() derrors.Error {
+	if conf.Port <= 0 {
+		return derrors.NewInvalidArgumentError("port must be specified")
+	}
+	if conf.InCluster && conf.Kubeconfig != "" {
+		return derrors.NewInvalidArgumentError("cannot specify kubeconfig when running in cluster")
+	}
+	if !conf.InCluster && conf.Kubeconfig == "" {
+		return derrors.NewInvalidArgumentError("one of in-cluster or kubeconfig should be specified")
+	}
+	if conf.Kubeconfig != "" {
+		f, err := os.Open(conf.Kubeconfig)
+		if err != nil {
+			return derrors.NewInvalidArgumentError(fmt.Sprintf("cannot open kubeconfig %s", conf.Kubeconfig), err)
+		}
+		f.Close()
+	}
+	return nil
+}
+
+// Print the current API configuration to the log.
+func (conf *Config) Print() {
+	log.Info().Int("port", conf.Port).Msg("gRPC port")
+	log.Info().Str("file", conf.Kubeconfig).Bool("in-cluster", conf.InCluster).Msg("kubeconfig")
+}
---
+++ internal/app/slave/service.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2019 Nalej - All Rights Reserved
+ */
+
+package slave
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/nalej/derrors"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"github.com/rs/zerolog/log"
+)
+
+// Service with configuration and gRPC server
+type Service struct {
+	Configuration *Config
+}
+
+func NewService(conf *Config) (*Service, derrors.Error) {
+	err := conf.Validate()
+	if err != nil {
+		log.Error().Msg("Invalid configuration")
+		return nil, err
+	}
+	conf.Print()
+
+	return &Service{
+		Configuration: conf,
+	}, nil
+}
+
+// Run the service, launch the REST service handler.
+func (s *Service) Run() derrors.Error {
+	// Start listening
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.Configuration.Port))
+	if err != nil {
+		return derrors.NewUnavailableError("failed to listen", err)
+	}
+
+	// Create managers and handler
+	// TBD
+
+	// Create server and register handler
+	server := grpc.NewServer()
+	// TBD: register handler
+
+	reflection.Register(server)
+	log.Info().Int("port", s.Configuration.Port).Msg("Launching gRPC server")
+	if err := server.Serve(lis); err != nil {
+		return derrors.NewUnavailableError("failed to serve", err)
+	}
+
+	return nil
+}
