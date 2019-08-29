dc857d542d6f6f8b895debd1187a68927f813e72
Gaizka Díaz <gdiaz@nalej.com>
2019-08-29 15:10:36 +0200

    fixed issue in address check

--- internal/app/monitoring-manager/cluster/client.go
+++ internal/app/monitoring-manager/cluster/client.go
@@ -32,6 +32,7 @@ type clusterClient struct {
 // one that creates stub clients
 func NewClusterClient(address string, params *AppClusterConnectParams) (*clusterClient, derrors.Error) {
 	var options []grpc.DialOption
+	var hostname string
 
 	log.Debug().Str("address", address).Interface("params", params).Msg("creating app cluster client")
 
@@ -41,8 +42,9 @@ func NewClusterClient(address string, params *AppClusterConnectParams) (*cluster
 
 	if params.UseTLS {
 		rootCAs := x509.NewCertPool()
-		hostname := strings.Split(address, ":")[0]
-		if len(hostname) != 2 {
+		splitHostname := strings.Split(address, ":")
+		if len(splitHostname) != 2 {
+			hostname = splitHostname[0]
 		} else {
 			return nil, derrors.NewInvalidArgumentError("server address incorrectly set")
 		}
