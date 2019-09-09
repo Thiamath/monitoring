50c47d322dd3a2ff71d6d4f749a455e020b692a2
Gaizka Díaz <gdiaz@nalej.com>
2019-09-09 16:45:26 +0200

    client cert part added

--- internal/app/monitoring-manager/cluster/client.go
+++ internal/app/monitoring-manager/cluster/client.go
@@ -56,6 +56,7 @@ func NewClusterClient(address string, params *AppClusterConnectParams) (*cluster
 		if params.CACertPath != "" {
 			log.Debug().Str("serverCertPath", params.CACertPath).Msg("loading server certificate")
 			serverCert, err := ioutil.ReadFile(params.CACertPath)
+			log.Debug().Interface("serverCert", serverCert).Msg("ca certificate")
 			if err != nil {
 				return nil, derrors.NewInternalError("Error loading server certificate")
 			}
@@ -63,7 +64,20 @@ func NewClusterClient(address string, params *AppClusterConnectParams) (*cluster
 			if !added {
 				return nil, derrors.NewInternalError("cannot add server certificate to the pool")
 			}
+			log.Debug().Interface("rootCAs", rootCAs).Msg("root cas added")
 			tlsConfig.RootCAs = rootCAs
+		}
+
+		if params.ClientCertPath != "" {
+			log.Debug().Str("clientCertPath", params.ClientCertPath).Msg("loading client certificate")
+			clientCert, err := tls.LoadX509KeyPair(fmt.Sprintf("%s/tls.crt", params.ClientCertPath),fmt.Sprintf("%s/tls.key", params.ClientCertPath))
+			if err != nil {
+				log.Error().Str("error", err.Error()).Msg("Error loading client certificate")
+				return nil, derrors.NewInternalError("Error loading client certificate")
+			}
+
+			tlsConfig.Certificates = []tls.Certificate{clientCert}
+			tlsConfig.BuildNameToCertificate()
 		}
 
 		log.Debug().Str("address", hostname).Bool("useTLS", params.UseTLS).Str("serverCertPath", params.CACertPath).Bool("skipServerCertValidation", params.SkipServerCertValidation).Msg("creating secure connection")
@@ -73,6 +87,7 @@ func NewClusterClient(address string, params *AppClusterConnectParams) (*cluster
 			tlsConfig.InsecureSkipVerify = true
 		}
 
+		//printRelevantTLSConfig(tlsConfig)
 		creds := credentials.NewTLS(tlsConfig)
 		log.Debug().Interface("creds", creds.Info()).Msg("Secure credentials")
 		options = append(options, grpc.WithTransportCredentials(creds))
@@ -97,6 +112,24 @@ func (c *clusterClient) Close() error {
 	}
 
 	return err
+}
+
+// printRelevantTLSConfig prints some relevant information from a TLS Config structure, namely:
+// ClientAuth, ServerName. RootCAs, Certificates and InsecureSkipVerify
+func printRelevantTLSConfig (c *tls.Config) {
+	if int(c.ClientAuth) != 0 {
+		log.Debug().Int("ClientAuth", int(c.ClientAuth)).Msg("client auth")
+	}
+	if c.ServerName != "" {
+		log.Debug().Str("ServerName", c.ServerName).Msg("server name")
+	}
+	if c.RootCAs != nil {
+		log.Debug().Interface("RootCAs", c.RootCAs).Msg("root cas")
+	}
+	if c.Certificates != nil {
+		log.Debug().Interface("Certificates", c.Certificates).Msg("certificates")
+	}
+	log.Debug().Bool("InsecureSkipVerify", c.InsecureSkipVerify).Msg("insecure skip verify")
 }
 
 // Add X509 certificate from a file to a pool
