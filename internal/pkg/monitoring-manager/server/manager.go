9ae0546c23146bdb8a9cc7b15f8d5cfa0054d8a8
Marcos Garcia <thiamath@gmail.com>
2019-12-02 10:49:44 +0100

    NP-2388 Preview.

--- internal/pkg/monitoring-manager/server/manager.go
+++ internal/pkg/monitoring-manager/server/manager.go
@@ -16,10 +16,11 @@
 
 // Manager implementation for cluster monitoring
 
-package cluster
+package server
 
 import (
 	"context"
+	"github.com/nalej/grpc-organization-go"
 	"github.com/nalej/monitoring/internal/pkg/monitoring-manager/clients"
 
 	"github.com/nalej/derrors"
@@ -58,7 +59,7 @@ func (m *Manager) getClusterClient(organizationId, clusterId string) (*clients.C
 }
 
 // Retrieve a summary of high level cluster resource availability
-func (m *Manager) GetClusterSummary(ctx context.Context, request *grpc_monitoring_go.ClusterSummaryRequest) (*grpc_monitoring_go.ClusterSummary, derrors.Error) {
+func (m *Manager) GetClusterSummary(ctx context.Context, request *grpc_monitoring_go.ClusterSummaryRequest) (*grpc_monitoring_go.ClusterSummary, error) {
 	client, derr := m.getClusterClient(request.GetOrganizationId(), request.GetClusterId())
 	if derr != nil {
 		return nil, derr
@@ -74,7 +75,7 @@ func (m *Manager) GetClusterSummary(ctx context.Context, request *grpc_monitorin
 }
 
 // Retrieve statistics on cluster with respect to platform resources
-func (m *Manager) GetClusterStats(ctx context.Context, request *grpc_monitoring_go.ClusterStatsRequest) (*grpc_monitoring_go.ClusterStats, derrors.Error) {
+func (m *Manager) GetClusterStats(ctx context.Context, request *grpc_monitoring_go.ClusterStatsRequest) (*grpc_monitoring_go.ClusterStats, error) {
 	client, derr := m.getClusterClient(request.GetOrganizationId(), request.GetClusterId())
 	if derr != nil {
 		return nil, derr
@@ -90,7 +91,7 @@ func (m *Manager) GetClusterStats(ctx context.Context, request *grpc_monitoring_
 }
 
 // Execute a query directly on the monitoring storage backend
-func (m *Manager) Query(ctx context.Context, request *grpc_monitoring_go.QueryRequest) (*grpc_monitoring_go.QueryResponse, derrors.Error) {
+func (m *Manager) Query(ctx context.Context, request *grpc_monitoring_go.QueryRequest) (*grpc_monitoring_go.QueryResponse, error) {
 	client, derr := m.getClusterClient(request.GetOrganizationId(), request.GetClusterId())
 	if derr != nil {
 		return nil, derr
@@ -103,4 +104,46 @@ func (m *Manager) Query(ctx context.Context, request *grpc_monitoring_go.QueryRe
 	}
 
 	return res, nil
+}
+
+func (m *Manager) GetOrganizationApplicationStats(ctx context.Context, request *grpc_monitoring_go.OrganizationApplicationStatsRequest) (*grpc_monitoring_go.OrganizationApplicationStatsResponse, error) {
+	clusterList, err := m.clustersClient.ListClusters(ctx, &grpc_organization_go.OrganizationId{OrganizationId: request.OrganizationId})
+	if err != nil {
+		return nil, derrors.NewFailedPreconditionError("could not find cluster list", err)
+	}
+
+	serviceInstanceStats := make([]*grpc_monitoring_go.OrganizationApplicationStats, 0)
+	orgAppStats := &grpc_monitoring_go.OrganizationApplicationStatsResponse{
+		ServiceInstanceStats: serviceInstanceStats,
+		Timestamp:            0,
+	}
+	for _, cluster := range clusterList.Clusters {
+		client, derr := m.getClusterClient(request.OrganizationId, cluster.ClusterId)
+		if derr != nil {
+			// TODO what to do if a cluster client cannot be created
+			continue
+		}
+		containerStats, err := client.GetContainerStats(ctx, nil)
+		if err != nil {
+			// TODO what to do if cluster do not respond with stats
+			continue
+		}
+		for _, stats := range containerStats.ContainerStats {
+			serviceInstanceStats = append(serviceInstanceStats, &grpc_monitoring_go.OrganizationApplicationStats{
+				OrganizationId: request.OrganizationId,
+				//OrganizationName:         organizationName, TODO
+				//AppInstanceId:            aooInstanceId, TODO
+				//AppInstanceName:          appInstanceNAme, TODO
+				//ServiceGroupInstanceId:   serviceGroupInstanceId, TODO
+				//ServiceGroupInstanceName: serviceGroupInstanceName, TODO
+				//ServiceInstanceId:        serviceInstanceId, TODO
+				//ServiceInstanceName:      serviceInstanceName, TODO
+				CpuMillicore: stats.CpuMillicore * cluster.MillicoresConversionFactor,
+				MemoryByte:   stats.MemoryByte,
+				StorageByte:  stats.StorageByte,
+			})
+		}
+	}
+
+	return orgAppStats, nil
 }
