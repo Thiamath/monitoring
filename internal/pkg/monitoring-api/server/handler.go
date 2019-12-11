6e1bc68ee5759fbc9baaea6720ae5a7e1ca4cacf
Marcos Garcia <thiamath@gmail.com>
2019-12-11 16:46:11 +0100

    NP-2384 Generate first approach of Handler and Manager.

---
+++ internal/pkg/monitoring-api/server/handler.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Nalej
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package server
+
+import (
+	"github.com/nalej/derrors"
+	"github.com/nalej/grpc-monitoring-go"
+	"golang.org/x/net/context"
+	"google.golang.org/genproto/googleapis/api/httpbody"
+)
+
+type Handler struct {
+	manager *Manager
+}
+
+func NewHandler(manager *Manager) (*Handler, derrors.Error) {
+	return &Handler{manager: manager}, nil
+}
+
+func (h *Handler) Metrics(context.Context, *grpc_monitoring_go.OrganizationApplicationStatsRequest) (*httpbody.HttpBody, error) {
+	panic("implement me")
+}
